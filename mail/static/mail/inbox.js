07d64cd6d9def7ff1b93b1db43a8405f5284955a
cheetiantey <37930535+cheetiantey@users.noreply.github.com>
2021-03-07 23:26:48 -0600

    fixed 'Send Mail' and 'Mailbox'

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -15,6 +15,27 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  
+  console.log("This is a test")
+
+  document.querySelector('#compose-submit').onclick = () => {
+    console.log("This is a second test")
+    fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: document.querySelector('#compose-recipients').value,
+        subject: document.querySelector('#compose-subject').value,
+        body: document.querySelector('#compose-body').value
+      })
+    })
+    .then(response => response.json())
+    .then(result => {
+      console.log(result)
+      console.log("This is a third test")
+      load_mailbox('inbox')
+
+    });
+  }
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -30,4 +51,29 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch('/emails/inbox')
+  .then(response => response.json())
+  .then(email => {
+    console.log(email)
+    email.forEach(email => {
+      // Email is unread - make background color white; Email is read - make background color red
+      if (email.read === false) {
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'white';
+        div.innerHTML = `<h4>Sender: ${email.sender}</h4>`;
+        div.innerHTML += `<h6>Subject line: ${email.subject}</h6>`;
+        div.innerHTML += `<h6>Timestamp: ${email.timestamp}</h6>`;
+        document.querySelector('#emails-view').append(div);
+      } else {
+        const div = document.createElement('div');
+        div.style.backgroundColor = 'red';
+        div.innerHTML = `<h4>Sender: ${email.sender}</h4>`;
+        div.innerHTML += `<h6>Subject line: ${email.subject}</h6>`;
+        div.innerHTML += `<h6>Timestamp: ${email.timestamp}</h6>`;
+        document.querySelector('#emails-view').append(div);
+      }
+      
+    });
+  });
 }
